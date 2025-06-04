5c0a53db32bdff2f6d808f7210a1ac3cef743eed
Emilio Andrade <emilio.andrade.375@gmail.com>
2025-06-04 07:29:36 -0600

    Archivo para configurar git hub

---
+++ setup.git.sh
@@ -0,0 +1,17 @@
+#Set this when using version control on a new computer 
+
+# Change with your github username
+git config --global user.name "Emilio Andrade"
+
+# Change with the email you register in github
+git config --global user.email "emilio.andrade.375@gmail.com"
+
+# You DONT need to change the following comands
+
+#If windows use:
+git config --global core.autocrlf true
+git config --global init.defaultBranch main
+git config --global core.editor "nano -w"
+
+# To get instructions about creating your ssh key and using it on github use the following link:
+https://swcarpentry.github.io/git-novice/07-github.html
